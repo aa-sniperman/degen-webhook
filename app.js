2da3d88fd52406621fcbd5e56f035fa6b7834dc4
tranduynhat0209 <nhatducmo@gmail.com>
2024-06-01 12:09:38 +0700

    fix: add sha256 verification

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
 const axios = require('axios');
 
 const app = express();
-const port = process.env.PORT || 12345;
+const port = process.env.PORT || 3000;
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+const GITHUB_WEBHOOK_SECRET = process.env.GITHUB_WEBHOOK_SECRET;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    verify: (req, res, buf, encoding) => {
+        const signature = `sha256=${crypto
+            .createHmac('sha256', GITHUB_WEBHOOK_SECRET)
+            .update(buf)
+            .digest('hex')}`;
+        req.signature = signature;
+    }
+}));
 
-app.post('/github-webhook', async (req, res) => {
+function verifySignature(req, res, next) {
+    const signature = req.headers['x-hub-signature-256'];
+    if (signature !== req.signature) {
+        return res.status(403).send('Request body was not signed or verification failed');
+    }
+    next();
+}
+
+app.post('/github-webhook', verifySignature, async (req, res) => {
     const githubEvent = req.headers['x-github-event'];
     const payload = req.body;
 
-    // Extract necessary details
     const repoName = payload.repository.full_name;
     const pusherName = payload.pusher ? payload.pusher.name : 'N/A';
     const commitMessage = payload.head_commit ? payload.head_commit.message : 'No commit message';
     const commitUrl = payload.head_commit ? payload.head_commit.url : 'No commit URL';
     const senderName = payload.sender ? payload.sender.login : 'N/A';
 
-    // Construct a decorated message to send to Telegram
     const message = `
 🔔 *New GitHub Event* 🔔
 
@@ -41,7 +57,7 @@ app.post('/github-webhook', async (req, res) => {
         await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
             chat_id: TELEGRAM_CHAT_ID,
             text: message,
-            parse_mode: 'Markdown' // Using Markdown to format the message
+            parse_mode: 'Markdown',
         });
         res.status(200).send('Event received and message sent to Telegram');
     } catch (error) {
